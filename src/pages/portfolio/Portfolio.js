616d2cbf8d797dfbfd14e148251523f2180d8a5f
Sarah Richens <sarahgrichens@gmail.com>
2023-05-02 17:04:58 -0500

    continue portfolio page adds

--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -1,7 +1,7 @@
 import './Portfolio.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCompactDisc } from '@fortawesome/pro-solid-svg-icons';
-import { FaGithub } from 'react-icons/fa';
+import { FaCloudRain, FaGithub, FaSun} from 'react-icons/fa';
 import { SiHeroku } from 'react-icons/si';
 
 const Portfolio = () => {
@@ -12,38 +12,47 @@ const Portfolio = () => {
                 
                     <div className="port-card1">
                         <div className="retro-content">
-                        <h3 className="retro-head">
-                            <span className="retro-text">RETR</span>
-                            <span className="retro-icon">
-                            <FontAwesomeIcon icon={faCompactDisc} spin />
-                                </span>
-                            <span className="retro-text">great</span>
-                        </h3>
-                        <a href="https://retro-great-1.herokuapp.com/">
-                            <p className="heroku-icon"><SiHeroku/></p>
-                        </a>
+                            <h3 className="retro-head">
+                                <span className="retro-text">RETR</span>
+                                <span className="retro-icon">
+                                <FontAwesomeIcon icon={faCompactDisc} spin />
+                                    </span>
+                                <span className="retro-text">great</span>
+                            </h3>
+                            <a href="https://retro-great-1.herokuapp.com/" target="blank">
+                                <p className="heroku-icon"><SiHeroku/></p>
+                            </a>
                         </div>
                     </div>
                
                 <div className="port-card2">
                     <div className="tech-content">
-                    <h3 className="tech-head">The Tech Blog</h3>
-                    <a href="https://techblog74.herokuapp.com/">
-                        <p className="heroku-icon"><SiHeroku/></p>
-                    </a>
+                        <h3 className="tech-head">The Tech Blog</h3>
+                        <a href="https://techblog74.herokuapp.com/" target="blank">
+                            <p className="heroku-icon"><SiHeroku/></p>
+                        </a>
                     </div>
                 </div>
                 <div className="port-card3">
                     <div className="dec-content">
-                        <h3 className="dec-head">
-                            <span className="dec-bar">Decisive</span>
-                        </h3>
-                        <a href="https://srichens.github.io/decisive/">
+                        <h3 className="dec-head">Decisive</h3>
+                        <a href="https://srichens.github.io/decisive/" target="blank">
                             <p className="github-icon"><FaGithub/></p>
                         </a>
                     </div>
                 </div>
-                <div className="port-card4">Weather Dashboard</div>
+                <div className="port-card4">
+                    <div className="weath-content">
+                        <h3 className="weath-head">Weather Dashboard</h3>
+                        <div className="weath-icons">
+                            <span className="sun"><FaSun /></span>
+                            <span className="rain"><FaCloudRain /></span>
+                         </div>
+                        <a href="https://srichens.github.io/weather-dashboard/" target="blank">
+                            <p className="github-icon"><FaGithub/></p>
+                        </a>
+                    </div>
+                </div>
                 <div className="port-card5">Note Taker</div>
                 <div className="port-card6">Code Quiz</div>
             </div>
