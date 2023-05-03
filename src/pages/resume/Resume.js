4d69bbed474016a0a473c5c4d1243036c81215b5
Sarah Richens <sarahgrichens@gmail.com>
2023-05-03 15:03:10 -0500

    start resume page, add pdf download

--- src/pages/resume/Resume.js
+++ src/pages/resume/Resume.js
@@ -1,9 +1,12 @@
 import './Resume.css';
+import "./assets/sarah-richens-resume.pdf"
 
 const Resume = () => {
     return (
         <div className="resume">
             <h2>Resume</h2>
+            <a href="https://drive.google.com/file/d/1lm5XwqTFQ1KyqgV2Ehwlmj2onNQlT7mI/view?usp=sharing" 
+            target="blank" download>Download Resume</a>
         </div>
     )
 };
