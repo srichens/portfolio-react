fbadc84dbd202c5ed35efeb6eb5f40d7a43424ba
Sarah Richens <sarahgrichens@gmail.com>
2023-05-04 09:36:24 -0500

    remove home page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import Main from './pages/main/Main';
-import Home from './pages/home/Home';
-import React, { useState } from 'react';
+//import Home from './pages/home/Home';
+//import React, { useState } from 'react';
 
 function App() { 
-  const [showHome, setShowHome] = useState(true); 
-  
   return (
     <div className="App">
-     { showHome?<Home setShowHome={setShowHome} /> : <Main/>  };  
-                 
+      <Main/>                    
     </div>
   );
 }
+//   const [showHome, setShowHome] = useState(true); 
+  
+//   return (
+//     <div className="App">
+//      { showHome?<Home setShowHome={setShowHome} /> : <Main/>  };  
+                 
+//     </div>
+//   );
+// }
 
 
 export default App;
