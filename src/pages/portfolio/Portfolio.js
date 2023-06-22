6358d22b5e5fa4110587f504ee861cf9ee66de4c
Sarah Richens <sarahgrichens@gmail.com>
2023-06-22 15:43:59 -0500

    add mixer and update styling

--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -1,6 +1,7 @@
 import './Portfolio.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCompactDisc, faCloudShowersHeavy } from '@fortawesome/pro-solid-svg-icons';
+import { faPaintbrushPencil} from '@fortawesome/pro-regular-svg-icons';
+import { faCompactDisc, faCloudShowersHeavy, faSun } from '@fortawesome/pro-solid-svg-icons';
 import { faPencil } from '@fortawesome/pro-duotone-svg-icons';
 import { FaGithub, FaSun} from 'react-icons/fa';
 import Navbar from '../../components/navbar/Navbar.js';
@@ -12,7 +13,25 @@ const Portfolio = () => {
             <Navbar />
         <div className="portfolio">
             <h2 className="port-head">Portfolio</h2>
-            <div className="port-container">                
+            <div className="port-container">    
+                <div className="port-card6">
+                    <div className="code-content">
+                        <a href="https://paint-mixer.herokuapp.com/" target="blank">
+                            <h3 className="code-head">
+                                <span className="m">m</span>
+                                <span className="i">i</span>
+                                <span className="login-icon">
+                                    <FontAwesomeIcon icon={faPaintbrushPencil} flip="vertical" />
+                                </span>
+                                <span className="e">e</span>
+                                <span className="r">r</span>
+                            </h3>
+                        </a>
+                        <a href="https://github.com/Gatewayss/mixer" target="blank">
+                            <p className="github-icon"><FaGithub/></p>
+                        </a>
+                    </div>
+                </div>            
                 <div className="port-card1">
                     <div className="retro-content">
                         <a href="https://retro-great-1.herokuapp.com/" target="blank">
@@ -32,17 +51,36 @@ const Portfolio = () => {
                             <p className="github-icon"><FaGithub/></p>
                         </a>
                     </div>
-                </div>               
+                </div>   
+                            
                 <div className="port-card2">
                     <div className="tech-content">
                         <a href="https://techblog74.herokuapp.com/" target="blank">
-                            <h3 className="tech-head">The Tech Blog</h3>
+                            <h3 className="tech-head">The Tech <br></br>Blog</h3>
                         </a>
                         <a href="https://github.com/srichens/tech-blog" target="blank">
                             <p className="github-icon"><FaGithub/></p>
                         </a>
                     </div>
                 </div>
+                <div className="port-card4">
+                    <div className="weath-content">
+                        <a href="https://srichens.github.io/weather-dashboard/" target="blank">
+                            <h3 className="weath-head">
+                                <span className="weath-icons">
+                                    <span className="sun"><FontAwesomeIcon icon={faSun} beatFade /></span>
+                                    Weather
+                                    <span className="rain"><FontAwesomeIcon icon={faCloudShowersHeavy} beatFade /></span>
+                                </span>
+                                Dashboard
+                            </h3>
+                        </a>                 
+                        <a href="https://github.com/srichens/weather-dashboard" target="blank">
+                            <p className="github-icon"><FaGithub/></p>
+                        </a>
+                    </div>
+                </div>
+
                 <div className="port-card3">
                     <div className="dec-content">
                         <a href="https://srichens.github.io/decisive/" target="blank">
@@ -53,33 +91,8 @@ const Portfolio = () => {
                         </a>
                     </div>
                 </div>
-                <div className="port-card6">
-                    <div className="code-content">
-                        <a href="https://srichens.github.io/code-quiz/" target="blank">
-                            <h3 className="code-head">Code Quiz</h3>
-                        </a>
-                        <a href="https://github.com/srichens/code-quiz" target="blank">
-                            <p className="github-black-icon"><FaGithub/></p>
-                        </a>
-                    </div>
-                </div>
-                <div className="port-card4">
-                    <div className="weath-content">
-                        <a href="https://srichens.github.io/weather-dashboard/" target="blank">
-                            <h3 className="weath-head">
-                                <span className="weath-icons">
-                                    <span className="sun"><FaSun /></span>
-                                    Weather
-                                    <span className="rain"><FontAwesomeIcon icon={faCloudShowersHeavy}/></span>
-                                </span>
-                                Dashboard
-                            </h3>
-                        </a>                 
-                        <a href="https://github.com/srichens/weather-dashboard" target="blank">
-                            <p className="github-icon"><FaGithub/></p>
-                        </a>
-                    </div>
-                </div>
+         
+     
                 <div className="port-card5">
                     <div className="note-content">
                         <a href="https://note-taker-srichens.herokuapp.com/" target="blank">
