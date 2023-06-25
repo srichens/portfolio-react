392d539c691f57ed03f11f0ea758fb8af6a046b3
Sarah Richens <sarahgrichens@gmail.com>
2023-06-25 16:33:29 -0500

    Update bio

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -15,7 +15,7 @@ const About = () => {
                 </div>
                 <div className="bio">
                     <h2 className="about-title">About Me</h2>
-                    <p>I recently earned a certificate in full-stack software engineering from the University of Minnesota after many years as an administrative professional. I'm looking forward to a new and interesting challenge, while continuing my dedication to excellence, hard work, and great customer service.</p>
+                    <p>I recently earned a certificate in full-stack software engineering from the University of Minnesota after many years as an administrative professional. I'm looking forward to a new and interesting challenge, while continuing my dedication to excellence, hard work, and great customer service in the work place. I am particularly interested in front-end development, where, along my obsessive need to "figure it out," I could also make use of my creative and artistic side.</p>
                 </div>               
             </div>
             <Footer />
